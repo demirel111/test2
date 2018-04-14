1855f2674da7b099f80daa543c1e1f0f07a95e08
Sulaymon <demirel6777@gmail.com>
2018-04-15 00:52:17 +0300

    commit from master

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,11 @@
+/**
+ * Date 15.04.2018
+ *
+ * @author Hursanov Sulaymon
+ * @version v1.0
+ **/
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hello, git from master");
+    }
+}
