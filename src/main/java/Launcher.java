d7435219ad2739c548bd4d2fd423df2fde00a92d
Sulaymon <demirel6777@gmail.com>
2018-04-15 00:55:50 +0300

    commit from developer branch

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -6,6 +6,6 @@
  **/
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("Hello, git from master");
+        System.out.println("Hello, git from developer branch");
     }
 }
